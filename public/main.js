3a101a101dc9ab3e91c3baa3393c11658cd23008
Tiller <ctiller@proforma.com>
2018-03-21 13:28:06 -0400

    Allow user to search by city or zip

--- public/main.js
+++ public/main.js
@@ -1,5 +1,31 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
+const body = document.querySelector("body");
+
+const searchInput = document.querySelector(".search-query");
+const API_KEY = 'b262969eaeb6ea12d97134e6a7589627'
+
+const getWeather = () => {
+
+  let weatherSection = document.createElement("section");
+
+  console.log(searchInput.value);
+  let searchParam = searchInput.value;
+
+  fetch(`http://api.openweathermap.org/data/2.5/weather?${!isNaN(searchParam) ? 'zip=' : 'q=' }${searchParam}&appid=${API_KEY}`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    let weatherDisplay = document.createElement("section");
+    weatherDisplay.textContent = JSON.stringify(data);
+    weatherSection.appendChild(weatherDisplay);
+  });
+
+  body.appendChild(weatherSection);
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const main = () => {
+  // document.querySelector('h1').textContent += '?'
+}
+
+document.addEventListener('DOMContentLoaded', main);
