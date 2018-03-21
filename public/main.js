9a8927929cb79ece3ba256670ce977d4f2e6dc86
Tiller <ctiller@proforma.com>
2018-03-21 14:40:10 -0400

    refactor fetch function and move output results for geolocation to top of mobile view.

--- public/main.js
+++ public/main.js
@@ -1,19 +1,32 @@
 const body = document.querySelector("body");
+const homeElement = document.querySelector(".home-element");
 
 const searchInput = document.querySelector(".search-query");
-const API_KEY = 'b262969eaeb6ea12d97134e6a7589627'
+const API_KEY = 'b262969eaeb6ea12d97134e6a7589627';
 
-const fetchAPIData = (searchParam, opt) => {
+// Handling the local storage and keeping track of the last search.
+const localStorage = window.localStorage;
+let tempData = JSON.parse(localStorage.getItem('lastSearch'));
+
+// To output it to the screen, we just run the 'createElement' function again.
+
+const fetchAPIData = (searchParam, opt, isMainElement) => {
   console.log(`latitude: ${searchParam}, longitude: ${opt}`);
 
   let weatherSection = document.createElement("section");
+  let searchQuery = `http://api.openweathermap.org/data/2.5/weather?${ opt ? `lat=${searchParam}&lon=${opt}` : !isNaN(searchParam) ? `zip=${searchParam}` : `q=${searchParam}` }&appid=${API_KEY}&units=imperial`;
 
-  fetch(`http://api.openweathermap.org/data/2.5/weather?${ opt ? `lat=${searchParam}&lon=${opt}` : !isNaN(searchParam) ? `zip=${searchParam}` : `q=${searchParam}` }&appid=${API_KEY}&units=imperial`)
+  fetch(searchQuery)
   .then((response) => {
     return response.json();
   })
   .then((data) => {
-    createAppendElement(data, weatherSection);
+    if(isMainElement) {
+      console.log("Our home!");
+      handleData(data, homeElement);
+    } else {
+      handleData(data, weatherSection);
+    }
   });
 
   body.appendChild(weatherSection);
@@ -23,6 +36,11 @@ const getWeather = () => {
   let searchParam = searchInput.value;
 
   fetchAPIData(searchParam);
+}
+
+const handleData = (data, element) => {
+  localStorage.setItem("lastSearch", JSON.stringify(data));
+  createAppendElement(data, element);  
 }
 
 const createAppendElement = (data, section) => {
@@ -35,7 +53,7 @@ const main = () => {
   if("geolocation" in navigator) {
     console.log("We can do it!");
     navigator.geolocation.getCurrentPosition((position) => {
-      fetchAPIData(position.coords.latitude.toFixed(9), position.coords.longitude.toFixed(9));
+      fetchAPIData(position.coords.latitude.toFixed(9), position.coords.longitude.toFixed(9), true);
     });
   } else {
     console.log("We cannot do it!");
